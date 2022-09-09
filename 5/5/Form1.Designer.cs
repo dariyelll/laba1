429e53c7220081df1f8953d5293a2e721f69c362
unknown <429193-21>
2022-09-09 15:38:30 +0300

    5lab

---
+++ 5/5/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace _5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(43, 49);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(235, 119);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ввод массива";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(8, 69);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(119, 27);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Случайно";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(10, 30);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(108, 27);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Вручную";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(374, 58);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Размер массива";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Lime;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(55, 36);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 17);
+            this.label2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.Color.Green;
+            this.button1.Location = new System.Drawing.Point(378, 167);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(204, 69);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Вычислить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(378, 86);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(4);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(165, 31);
+            this.numericUpDown1.TabIndex = 4;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(43, 276);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(714, 109);
+            this.dataGridView1.TabIndex = 5;
+            this.dataGridView1.EditingControlShowing += new System.Windows.Forms.DataGridViewEditingControlShowingEventHandler(this.dataGridView1_EditingControlShowing);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(374, 211);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 23);
+            this.label3.TabIndex = 6;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Lime;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(43, 415);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(714, 187);
+            this.panel1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(813, 643);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+     
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ 5/5/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace _5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int n; //переменная для количества элементов массива
+        Random r = new Random();
+
+        /// <summary>
+        /// Метод поиска элементов, кратных своему порядковому номеру.
+        /// </summary>   
+        /// <param name="a">массив для поиска кратных</param>
+        /// <returns>произведение элементов, кратных своему порядковому номеру</returns>
+        List<int> Multiplication(int[] a)
+        {
+            List<int> multi = new List<int>();
+            // Первый элемент массива всегда будет кратен своему порядковому номеру, тк любое число делится на 1, поэтому начинаем со второго
+            multi.Add(a[0]);
+            multi.Add(1);
+            
+            for (int i = 1; i < a.Length; i++)
+            {
+                // Если элемент кратен своему порядковому номеру, добавить в список и посчитать общее количество.
+                if (a[i] % (i + 1) == 0)
+                {
+                    multi[0] *= a[i];
+                    multi[1] += 1;
+                }
+            }
+            return multi;
+        }
+
+        /// <summary>
+        /// Кнопка "Вычислить" - метод удаления элементов, кратных своему порядковому номеру.
+        /// </summary>
+        /// <param name="sender">указатель на button1</param>
+        /// <param name="e">дополнительный аргумент</param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Очистка поля результата.
+            label2.Text = "";
+            // Выделение памяти под динамический массив.
+            int[] a = new int[n];
+            try
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    // Если одна или несколько ячеек таблицы пустые, выводим сообщение об ошибке.
+                    if (dataGridView1.Rows[0].Cells[i].Value == null) throw new Exception("Заполните все ячейки!");
+                    // Значения из таблицы помещаем в массив.
+                    a[i] = Convert.ToInt32(dataGridView1.Rows[0].Cells[i].Value);
+                }
+                // Вызов метода для поиска элементов, кратных своему порядковому номеру.
+                List<int> multi = Multiplication(a);
+
+                // Новый массив без элементов, кратных своему порядковому номеру.
+                int[] aNew = new int[a.Length - multi[1]];
+                // Номер текущего элемента нового массива.
+                int j = 0;
+
+                for (int i = 1; i < n; i++)
+                {
+                    // Если элемент является кратным своему порядковому номеру, пропускаем его.
+                    if (a[i] % (i+1) == 0) continue;
+
+                    // Заполняем новый массив элементами старого.
+                    aNew[j] = a[i];
+                    // Удаление элементов производится путём сдвига вперёд.
+                    j++;
+                }
+
+                // Устанавливаем количество колонок без учёта элементов, кратных своему порядковому номеру.
+                dataGridView1.ColumnCount = aNew.Length;
+                for (int i = 0; i < aNew.Length; i++)
+                {
+                    // Заполняем таблицу новым массивом.
+                    dataGridView1.Rows[0].Cells[i].Value = aNew[i];
+                }
+
+                if (multi[1] == 1)
+                    label2.Text = "Нет элементов, кратных заданному числу";
+                else
+                {
+
+                    label2.Text = "Произведение элементов массива, кратных своему порядковому номеру = " + multi[0];
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Метод интерактивного изменения способа ввода массива.
+        /// </summary>
+        /// <param name="sender">указатель на radioButton1</param>
+        /// <param name="e">дополнительный аргумент</param>
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            // Очистка поля результата.
+            label2.Text = "";
+            n = Convert.ToInt32(numericUpDown1.Value);
+            dataGridView1.RowCount = 1;
+            // Устанавливаем количество колонок.
+            dataGridView1.ColumnCount = n;
+            for (int i = 0; i < n; i++)
+            {
+                // Строка заголовков столбцов.
+                dataGridView1.Columns[i].Name = (i+1).ToString();
+                if (radioButton2.Checked)
+                    // Заполняем таблицу случайными значениями.
+                    dataGridView1.Rows[0].Cells[i].Value = r.Next(50, 101).ToString();
+                else
+                    dataGridView1.Rows[0].Cells[i].Value = 0;
+            }
+
+        }
+
+        /// <summary>
+        /// Метод интерактивного изменения размера таблицы.
+        /// </summary>
+        /// <param name="sender">указатель на numericUpDown1</param>
+        /// <param name="e">дополнительный аргумент</param>
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            n = Convert.ToInt32(numericUpDown1.Value);
+            // Устанавливаем количество колонок.
+            dataGridView1.ColumnCount = n;
+
+            for (int i = 0; i < n; i++)
+            {
+                // Строка заголовков столбцов.
+                dataGridView1.Columns[i].Name = (i+1).ToString();
+                // Новые ячейки заполняем нулями.
+                if (dataGridView1.Rows[0].Cells[i].Value == null) dataGridView1.Rows[0].Cells[i].Value = 0;
+            }
+
+        }
+
+        /// <summary>
+        /// Событие проверки нажимаемых клавиш при отображении таблицы для редактирования ячейки.
+        /// </summary>
+        /// <param name="sender">указатель на dataGridView1</param>
+        /// <param name="e">дополнительный аргумент</param>
+        private void dataGridView1_EditingControlShowing(object sender, DataGridViewEditingControlShowingEventArgs e)
+        {
+            e.Control.KeyPress += new KeyPressEventHandler(myKeyPress);
+        }
+
+        /// <summary>
+        /// Метод контроля вводимых данных в таблицу.
+        /// </summary>
+        /// <param name="sender">указатель на dataGridView1</param>
+        /// <param name="e">дополнительный аргумент</param>
+
+        private void myKeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Допустимыми являются только цифры и минус, можно стирать символы в поле ввода, используя клавишу BackSpace.
+            if (e.KeyChar >= '0' && e.KeyChar <= '9' || e.KeyChar == '-' || e.KeyChar == (char)Keys.Back) e.Handled = false;
+            else e.Handled = true;
+        }
+
+
+    }
+}
